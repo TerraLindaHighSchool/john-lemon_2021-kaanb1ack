44de6fa093102ae008838a01e1e94c49128a8aa0
kaanb1ack <89489424+kaanb1ack@users.noreply.github.com>
2022-01-11 15:36:55 -0800

    3

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+    private Quaternion rotation;
+    private bool isWalking;
+
+    [SerializeField] private float turnSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        rotation = Quaternion.identity();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnAnimatorMove()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
