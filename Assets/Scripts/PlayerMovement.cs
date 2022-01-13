f8232ceaf3741f7f503eba6930e46d3b6115a6c5
kaanb1ack <89489424+kaanb1ack@users.noreply.github.com>
2022-01-13 15:31:00 -0800

    grntntHT

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,17 +17,32 @@ public class NewBehaviourScript : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        rotation = Quaternion.identity();
+        rotation = Quaternion.identity;
     }
 
     void FixedUpdate()
     {
-        
-    }
+        //Get user input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
+        //Use user input to set direction player will move
+        moveDirection.Set(horizontal, 0f, vertical);
+        moveDirection.Normalize();
+
+        //Set animatorto walking or idle depending on user input 
+        isWalking = !(Mathf.Approximately(horizontal, 0f) && !Mathf.Approximately(vertical, 0f));
+        animator.SetBool("IsWalking", isWalking);
+
+       
+        //assign rotation towards move direction
+        Vector3 desiredDirection = Vector3.RotateTowards(transform.position, moveDirection, turnSpeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredDirection);
+    }  
     private void OnAnimatorMove()
-    { 
-        
+    {
+        rb.MovePosition(rb.position + moveDirection * animator.deltaPosition.magnitude);
+        rb.MoveRotation(rotation);
     }
 
     // Update is called once per frame
