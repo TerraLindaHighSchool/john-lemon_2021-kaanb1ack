396d20ef88dbfb5f9a758572047c7856853f0549
kaanb1ack <89489424+kaanb1ack@users.noreply.github.com>
2022-01-25 15:35:54 -0800

    ending

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody rb;
